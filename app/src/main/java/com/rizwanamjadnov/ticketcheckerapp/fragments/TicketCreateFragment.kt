c9f09862a0bac2f499e7cfcccd137b10b9fbb67d
RizwanAmjad <rizwanamjadms@gmal.com>
2022-01-25 14:54:28 +0500

    Completed the UI for Ticket Create Fragment

--- app/src/main/java/com/rizwanamjadnov/ticketcheckerapp/fragments/TicketCreateFragment.kt
+++ app/src/main/java/com/rizwanamjadnov/ticketcheckerapp/fragments/TicketCreateFragment.kt
@@ -5,16 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.rizwanamjadnov.ticketcheckerapp.R
 
 class TicketCreateFragment : Fragment() {
+    private lateinit var ticketTitleText: EditText
+    private lateinit var ticketDateText: EditText
+    private lateinit var maxScansText: EditText
+    private lateinit var createTicketButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticket_create, container, false)
+        val view = inflater.inflate(R.layout.fragment_ticket_create, container, false)
+
+        ticketTitleText = view.findViewById(R.id.ticketTitleText)
+        ticketDateText = view.findViewById(R.id.ticketDateText)
+        maxScansText = view.findViewById(R.id.maxScansText)
+        createTicketButton = view.findViewById(R.id.createTicketButton)
+
+        createTicketButton.setOnClickListener{
+            Toast.makeText(context, "Ok", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 
 }
---
+++ app/src/main/java/com/rizwanamjadnov/ticketcheckerapp/models/TicketModel.kt
@@ -0,0 +1,9 @@
+package com.rizwanamjadnov.ticketcheckerapp.models
+
+import java.util.*
+
+class TicketModel (
+    val ticketTitle:String,
+    val ticketDate: String,
+    val maxScans: Int) {
+}
